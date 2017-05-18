143b08f372139a8b011dfd91c01db3cf0d70817d
sebastiennelissen <sebastien.nelissen.1@ens.etsmtl.ca>
2017-05-18 22:06:37 +0200

    implement et pas private

--- src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
+++ src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
@@ -3,15 +3,16 @@ package src;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Properties;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AjaxServlet extends HttpServlet {
+public class AjaxServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
 
-	private static final long serialVersionUID = -8308514275340556375L;
+	static final long serialVersionUID = -8308514275340556375L;
 
 	public AjaxServlet() { } 
 
